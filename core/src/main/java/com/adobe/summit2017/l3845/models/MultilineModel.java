f943d8ef91eaf8dadfffb056ea80e7440f450ce2
schoger <schoger@adobe.com>
2017-02-21 16:34:27 +0100

    changed from List to Array, calculation shows number of lines

--- core/src/main/java/com/adobe/summit2017/l3845/models/MultilineModel.java
+++ core/src/main/java/com/adobe/summit2017/l3845/models/MultilineModel.java
@@ -1,22 +1,18 @@
 package com.adobe.summit2017.l3845.models;
 
-import java.util.List;
-
 import javax.inject.Inject;
 
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.Optional;
 
 @Model(adaptables=Resource.class)
 public class MultilineModel {
 
-	@Inject @Optional
-	private List<String> lines;
+	@Inject @Default(values={})
+	private String[] lines;
 	
-	public float getCalculation() {
-		float numLines = lines.size();
-		
-		return numLines / (numLines - 5) * -1; // will throw ArithmeticException (divide by zero)
+	public int getNumLines() { 
+		return lines.length; // will throw NPE if 'lines' is not initialized
 	}
 }
--- core/src/test/java/com/adobe/summit2017/l3845/models/MultilineModelTest.java
+++ core/src/test/java/com/adobe/summit2017/l3845/models/MultilineModelTest.java
@@ -1,46 +1,32 @@
 package com.adobe.summit2017.l3845.models;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import java.util.Arrays;
-import java.util.Collections;
-
-import org.junit.Before;
 import org.junit.Test;
 
 import junitx.util.PrivateAccessor;
 
 public class MultilineModelTest {
 
-	private MultilineModel model;
-	
-	@Before
-	public void setUp() throws Exception {
-		model = new MultilineModel();
-	}
-	  
+	private MultilineModel model = new MultilineModel();
+		  
 	@Test
 	public void testGetCalculationSuccess() throws NoSuchFieldException {
 		
-		PrivateAccessor.setField(model, "lines", Collections.<String>emptyList());
-		assertEquals(0, model.getCalculation(), 0.01);
+		PrivateAccessor.setField(model, "lines", new String[0]);
+		assertEquals(0, model.getNumLines());
 		
-		PrivateAccessor.setField(model, "lines", Arrays.asList(new String[] {"one"}));
-		assertEquals(0.25, model.getCalculation(), 0.01);
-				
-		PrivateAccessor.setField(model, "lines", Arrays.asList(new String[] {"one", "two", "three", "four"}));
-		assertEquals(4, model.getCalculation(), 0.01);
-		
-		PrivateAccessor.setField(model, "lines", Arrays.asList(new String[] {"one", "two", "three", "four", "five", "six"}));
-		assertEquals(-6, model.getCalculation(), 0.01);
-		
-	}
-
-	@Test(expected=ArithmeticException.class)
-	public void testGetCalculationException() throws NoSuchFieldException {
-
-		PrivateAccessor.setField(model, "lines", Arrays.asList(new String[] {"one", "two", "three", "four", "five"}));
-		model.getCalculation();
+		PrivateAccessor.setField(model, "lines", new String[] {"one"});
+		assertEquals(1, model.getNumLines());
 
 	}
+
+	@Test(expected=NullPointerException.class)
+	public void testGetCalculationNPE() throws NoSuchFieldException {
+
+		PrivateAccessor.setField(model, "lines", null);
+		model.getNumLines();
+
+	}
+
 }
